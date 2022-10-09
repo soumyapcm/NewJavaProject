0e94cd11e49a0b3189eaf7141e00b59c0ca238c5
eldos <eldos@LAPTOP-KQ5SBNN8>
2022-10-09 17:34:39 +0530

    Commiting hello world

---
+++ MyFirstJavaProject/src/com/java/demo/Calculator.java
@@ -0,0 +1,30 @@
+package com.java.demo;
+
+public class Calculator {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//}
+	int num1,num2;
+	int addition(int num1,int num2)
+	{
+		int result = num1+num2;
+		return result;		
+	}
+	int Substration(int num1,int num2)
+	{
+		int result = num1-num2;
+		return result;		
+	}
+	int multiplication(int num1,int num2)
+	{
+		int result = num1*num2;
+		return result;		
+	}
+	int division(int num1,int num2)
+	{
+		int result = num1/num2;
+		return result;		
+	}
+}
---
+++ MyFirstJavaProject/src/com/java/demo/solution.java
@@ -0,0 +1,37 @@
+package com.java.demo;
+
+public class solution {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+	}
+	}
+
+
+
+/*
+ * //import java.util.Scanner;
+Calculator calcu=new Calculator();
+Scanner sc=new Scanner(System.in);
+int num1=sc.nextInt();
+int num2=sc.nextInt();
+
+int sum=calcu.addition(num1, num2);
+System.out.println(sum);
+
+int sub=calcu.Substration(num1, num2);
+System.out.println(sub);
+
+int multi=calcu.multiplication(num1, num2);
+System.out.println(multi);
+
+try {
+int divi=calcu.division(num1, num2);
+System.out.println(divi);
+}catch(Exception ex)
+{System.out.println("Exception occured");
+}
+sc.close();
+*/
+
